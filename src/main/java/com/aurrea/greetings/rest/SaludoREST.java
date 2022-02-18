45f75b925b1105dcfd8917a73c709020a0930bc4
Andrea Mariana Urrea Urias <andrea.urrea@neoris.com>
2022-02-18 01:02:13 -0700

    Elimine exc handler

--- src/main/java/com/aurrea/greetings/rest/SaludoREST.java
+++ src/main/java/com/aurrea/greetings/rest/SaludoREST.java
@@ -4,9 +4,9 @@ package com.aurrea.greetings.rest;
 import java.util.List;
 import java.util.Optional;
 
-import org.postgresql.util.PSQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
